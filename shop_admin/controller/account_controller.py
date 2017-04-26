507fd1bd288a14997c00ba0a43b1a9ebb0dcc936
husonchen <1092441751@qq.com>
2017-04-26 23:25:05 +0800

    充值记录查看

---
+++ shop_admin/controller/account_controller.py
@@ -0,0 +1,18 @@
+from django_url_framework.controller import ActionController
+from shop_admin.util.TplHelper import *
+from shop_admin.model.shop_account import ShopAccount
+from shop_admin.model.charge_history import ChargeHistory
+from django.http import  HttpResponseRedirect
+import math
+
+class AccountController(ActionController):
+
+    def index(self,request):
+        user = request.session['user']
+        shopId = user.shop_id
+        account = ShopAccount.objects.filter(shop_id=shopId, del_flag=False).all()[0]
+        historys = ChargeHistory.objects.filter(shop_id=shopId).order_by('-create_time')[0:10]
+        num = ShopAccount.objects.filter(shop_id=shopId, del_flag=False).count()
+        pageNum = math.ceil(float(num) / 10)
+        return getTpl({'account':account,'historys': historys, 'pageNum': pageNum}, 'account/index')
+
