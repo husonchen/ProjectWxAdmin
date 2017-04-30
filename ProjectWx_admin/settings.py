c2f8cb0cda4f88ddee06c47f8c1d66804f50f048
husonchen <1092441751@qq.com>
2017-04-30 18:59:11 +0800

    上传功能

--- ProjectWx_admin/settings.py
+++ ProjectWx_admin/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME' : 'xiaob',
         'USER' : 'xiaob',
         'PASSWORD' : 'skdfjkasdf',
-        'HOST' : 'localhost',
+        'HOST' : '10.11.199.67',
         'PORT' : '3306',
         'default-character-set' : 'utf8',
     }
--- shop_admin/controller/dashboard_controller.py
+++ shop_admin/controller/dashboard_controller.py
@@ -1,6 +1,9 @@
+# encoding=utf-8
 from django_url_framework.controller import ActionController
 from shop_admin.util.TplHelper import *
 from django.http import  HttpResponseRedirect
+from django.shortcuts import render
+from shop_admin.model.order_ids import OrderIds
 
 class DashboardController(ActionController):
 
@@ -9,4 +12,21 @@ class DashboardController(ActionController):
         #     return HttpResponseRedirect('/login/')
         user = request.session['user']
         c = {"userName":user.shop_name}
-        return getTpl(c,'dashboard/index')
+        # return getTpl(c,'dashboard/index')
+        return render(request, 'dashboard/index.html', c)
+
+    def upload_ids(self,request):
+        if request.method == 'POST':
+            user = request.session['user']
+            myfile = request.FILES
+            content = myfile['myfile'].read()
+            # print content
+            querysetlist = []
+            lines = content.split('\r\n')
+            for line in lines[1:len(lines)]:
+                if line == '':
+                    continue
+                querysetlist.append(OrderIds(order_id=line,shop_id=user.shop_id,create_time=None))
+            OrderIds.objects.bulk_create(querysetlist,500)
+            return 'OK'
+        return 'Not Ok'
