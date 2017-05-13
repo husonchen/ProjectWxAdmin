ad48a0ebb193cfe6796f8a58e6885574a160cb9c
huson.chen <huson.chen@vipshop.com>
2017-05-14 00:21:24 +0800

    充值脚本

---
+++ charge_money.py
@@ -0,0 +1,12 @@
+#this file is used for admin charge user money
+import MySQLdb as mdb
+shop_id = int(raw_input('shop_id: '))
+#yuan
+money = int(raw_input('money(yuan): '))
+con = mdb.connect('localhost', 'xiaob', 'skdfjkasdf', 'xiaob')
+cur = con.cursor()
+money = money * 100
+cur.execute('Insert into charge_history(shop_id, amount, create_time) VALUES (%d,%d,null)'%(shop_id,money))
+cur.execute('Update shop_account set balance=balance+%d where shop_id=%d'%(money,shop_id))
+con.commit()
+con.close()
