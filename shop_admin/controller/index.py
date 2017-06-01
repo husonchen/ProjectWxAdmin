f6fac193c63cc11c9dd17993f3328b8b0bc74fb5
husonchen <1092441751@qq.com>
2017-06-01 23:53:51 +0800

    index重定向

--- shop_admin/controller/index.py
+++ shop_admin/controller/index.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 def hello(request):
-    return HttpResponse("Hello world")
+    return HttpResponseRedirect('/dashboard/')
