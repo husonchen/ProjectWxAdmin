9303fbc2c87f57260431bd0dc567324f0d22e0c4
husonchen <1092441751@qq.com>
2017-06-17 01:03:15 +0800

    解决时区问题

--- ProjectWx_admin/settings.py
+++ ProjectWx_admin/settings.py
@@ -141,13 +141,13 @@ CACHES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
