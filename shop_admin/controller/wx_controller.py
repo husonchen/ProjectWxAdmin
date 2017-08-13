1f2cb9f8b081d46e07402dafd7bc62fe31acfe7f
husonchen <1092441751@qq.com>
2017-08-14 02:18:32 +0800

    debug

--- shop_admin/controller/wx_controller.py
+++ shop_admin/controller/wx_controller.py
@@ -1,7 +1,7 @@
 from django_url_framework.controller import ActionController
 import xml.etree.ElementTree as ET
 from shop_admin.wx.WXBizMsgCrypt import WXBizMsgCrypt
-from ProjectWx_admin.settings import APPID,TOKEN
+from ProjectWx_admin.settings import APPID,TOKEN,encodingAESKey
 import logging
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,15 +11,15 @@ class WxController(ActionController):
     @csrf_exempt
     def open(self,request):
         logger.info('wx open')
-        # signature = request.GET['signature']
-        # timestamp = request.GET['timestamp']
+        signature = request.GET['signature']
+        timestamp = request.GET['timestamp']
         nonce = request.GET['nonce']
         # encrypt_type = request.GET['encrypt_type']
-        # msg_signature = request.GET['msg_signature']
+        msg_signature = request.GET['msg_signature']
         data = request.body
         logger.info(data)
-        encryp_test = WXBizMsgCrypt(TOKEN, data, APPID)
-        ret, encrypt_xml = encryp_test.EncryptMsg(encryp_test, nonce)
+        encryp_test = WXBizMsgCrypt(TOKEN, encodingAESKey, APPID)
+        ret, encrypt_xml = encryp_test.DecryptMsg(data,msg_signature,timestamp,nonce)
 
         xml = ET.fromstring(encrypt_xml)
         appid = xml.find('AppId').text
